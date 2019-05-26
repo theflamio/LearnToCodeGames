524b9ed15143a3ac2186ec277a81a7eda52a2851
Flemming Christensen <flamekfor@gmail.com>
2019-05-26 22:09:35 +0200

    RollzaBall first lvl done
    WIP start screen and end screen
    
    Need enermies

--- RollzaBall/Assets/scripts/PlayerController.cs
+++ RollzaBall/Assets/scripts/PlayerController.cs
@@ -1,15 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update()
-    {
-        if (Input.GetButtonDown(")
-        {
+    
+    public float speed = 100;
 
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+
+    private int gameScore = 0;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        SetScoreText();
+        winText.text = "";
+    }
+
+    void FixedUpdate() {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // addforce to vector x y and z
+        Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            gameScore++;
+            SetScoreText();
         }
     }
+
+    void SetScoreText(){
+        scoreText.text = "Score: " + gameScore.ToString();
+        if (gameScore >= 8)
+        {
+            winText.text = "You win !";
+        }
+    }       
 }   
 
