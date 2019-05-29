9838e39e1304c0087c856ccbb9586cc5e227261e
Flemming Christensen <flamekfor@gmail.com>
2019-05-29 14:20:36 +0200

    Added Start menu
    WIP pause menu

---
+++ RollzaBall/Assets/scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+}
