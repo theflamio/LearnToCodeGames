fa4770ee0ec4de4bd8aa385d1796ff45f9842d15
Flemming Christensen <flamekfor@gmail.com>
2020-05-09 21:57:36 +0200

    Added Terrain and player with basic controllers.
    WIP player cant jump very good yet.
    
    WIP need landscape in the lvl

---
+++ GameMenuTemplate/Assets/scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public float mouseSensitivity = 100;
+
+    public Transform playerBody;
+
+    private float xRotation = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;        
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; //follows the rigth rotation when moving
+        xRotation = Mathf.Clamp(xRotation, -90, 90); //prevents player flipping around the axis when looking
+
+        transform.localRotation = UnityEngine.Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(UnityEngine.Vector3.up * mouseX);
+    }
+}
---
+++ GameMenuTemplate/Assets/scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+   
+    void Update()
+    {
+        checkIfPlayerIsGrounded();
+
+        movementControlles();
+
+        playerJump();
+
+        GravityOnPlayer();
+    }
+
+    void checkIfPlayerIsGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+
+    void movementControlles()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void playerJump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    void GravityOnPlayer()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
